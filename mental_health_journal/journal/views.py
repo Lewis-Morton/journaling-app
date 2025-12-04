a215bd5704dee3be312233bbf7a253528c9669c7
Lewis <lewiswilliammorton@outlook.com>
2025-12-04 14:20:41 +0000

    Update views to include an option to list all journal entries and also add authentication to relevant views

--- mental_health_journal/journal/views.py
+++ mental_health_journal/journal/views.py
@@ -16,22 +16,53 @@ from django_ratelimit.decorators import ratelimit
 # Views for journalentry model
 
 # These views handle CRUD operations on journal entries
+
+# Custom permission to ensure users can only access their own journals
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
+
 @method_decorator(ratelimit(key='ip', rate='10/m', block=True), name='dispatch')
+class JournalListAPIView(generics.ListAPIView):
+    serializer_class = JournalEntrySerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    # Return only journals belonging to authenticated user and list in order created by
+    def get_queryset(self):
+        return JournalEntry.objects.filter(user=self.request.user).order_by('-create_at')
+
 class JournalCreateAPIView(generics.CreateAPIView):
     queryset = JournalEntry.objects.all()
     serializer_class = JournalEntrySerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    # Set the user to the authenticated user
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class JournalDetailAPIView(generics.RetrieveAPIView):
     queryset = JournalEntry.objects.all()
     serializer_class = JournalEntrySerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+    # Return only journals belonging to the authenticated user
+    def get_queryset(self):
+        return JournalEntry.objects.filter(user=self.request.user)
 
 class JournalUpdateAPIView(generics.UpdateAPIView):
     queryset = JournalEntry.objects.all()
     serializer_class = JournalEntrySerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return JournalEntry.objects.filter(user=self.request.user)
 
 class JournalDestroyAPIView(generics.DestroyAPIView):
     queryset = JournalEntry.objects.all()
     serializer_class = JournalEntrySerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return JournalEntry.objects.filter(user=self.request.user)
 
 # User Views
 
@@ -48,6 +79,7 @@ class UserRegisterAPIView(generics.CreateAPIView):
 class UserListAPIView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
     permission_classes = [permissions.IsAdminUser]
 
 # Retieves and updates user profile (self-only)
